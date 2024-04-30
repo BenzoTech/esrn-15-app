099116a81bc0279bd52ee0c91b07864591efa393
Benzo Tech <ardiles.md@gmail.com>
2024-04-30 09:36:08 -0300

    initial commit

---
+++ app/src/main/java/com/benzo/esrn15/ui/calendario/CalendarioFragment.kt
@@ -0,0 +1,22 @@
+package com.benzo.esrn15.ui.calendario
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.benzo.esrn15.databinding.FragmentCalendario3Binding
+
+class CalendarioFragment : Fragment() {
+    private var _binding: FragmentCalendario3Binding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCalendario3Binding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
